5389c558191ed85ef6dd35342e1fa413710580ce
Sam Toledano <samueltoledano94@gmail.com>
2025-03-28 12:23:46 +0100

    Premier commit

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', event => {
+    console.log('Service Worker installé ✅');
+    self.skipWaiting();
+  });
+  
+  self.addEventListener('activate', event => {
+    console.log('Service Worker activé ✅');
+  });
+  
+  self.addEventListener('fetch', event => {
+    event.respondWith(fetch(event.request));
+  });
+  
