3922cb3795cd91750eaa36b88bab3c0dcd688f47
Sam Toledano <samueltoledano94@gmail.com>
2025-03-31 12:09:07 +0200

    modification rapide

---
+++ assets/js/visit.js
@@ -0,0 +1,8 @@
+document.getElementById('tags-btn').addEventListener('click', () => {
+    window.location.href = 'tags.html';
+  });
+  
+  document.getElementById('theme-btn').addEventListener('click', () => {
+    window.location.href = 'theme.html';
+  });
+  
