32e3aaced34ad049678dd791acb8104d201f4bf9
Sam Toledano <samueltoledano94@gmail.com>
2025-04-14 11:16:29 +0200

    retour_hier

--- assets/js/visit.js
+++ assets/js/visit.js
@@ -1,7 +1,40 @@
-document.getElementById('tags-btn').addEventListener('click', () => {
-  window.location.href = '../pages/tags.html';
+
+function addSwipeListener(element, callback) {
+  let touchStartX = 0;
+  let touchEndX = 0;
+
+  element.addEventListener("touchstart", (e) => {
+    touchStartX = e.changedTouches[0].screenX;
+  });
+
+  element.addEventListener("touchend", (e) => {
+    touchEndX = e.changedTouches[0].screenX;
+    handleSwipe();
+  });
+
+  function handleSwipe() {
+    const swipeDistance = touchEndX - touchStartX;
+    if (swipeDistance > 50) {
+      // vers la droite
+      callback();
+    }
+  }
+}
+
+// Clic normal
+document.getElementById("tags-btn").addEventListener("click", () => {
+  window.location.href = "../pages/tags.html";
 });
 
-document.getElementById('theme-btn').addEventListener('click', () => {
-  window.location.href = '../pages/themes.html';
+document.getElementById("theme-btn").addEventListener("click", () => {
+  window.location.href = "../pages/themes.html";
+});
+
+// Swipe vers la droite
+addSwipeListener(document.getElementById("tags-btn"), () => {
+  window.location.href = "../pages/tags.html";
+});
+
+addSwipeListener(document.getElementById("theme-btn"), () => {
+  window.location.href = "../pages/themes.html";
 });
