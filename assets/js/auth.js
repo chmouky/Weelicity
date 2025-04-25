cbd63f2ab0a8270e26a5064f46a23f80202f3617
Sam Toledano <samueltoledano94@gmail.com>
2025-04-25 11:44:41 +0200

    retour_hier

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,5 +1,6 @@
 // 📁 assets/js/auth.js
-import createAuth0Client from 'https://cdn.jsdelivr.net/npm/@auth0/auth0-spa-js@1.20.2/dist/auth0-spa-js.production.esm.js';
+
+import createAuth0Client from 'https://cdn.jsdelivr.net/npm/@auth0/auth0-spa-js@1.20.2/dist/auth0-spa-js.production.esm.min.js';
 
 let auth0Client = null;
 let auth0Ready = false;
@@ -14,7 +15,6 @@ const auth0Config = {
 export async function initAuth() {
   if (!auth0InitPromise) {
     auth0InitPromise = createAuth0Client(auth0Config)
- // ✅ ici window.
       .then(client => {
         auth0Client = client;
         auth0Ready = true;
@@ -35,7 +35,7 @@ export function isUserLoggedIn() {
 }
 
 export async function handleRedirectCallback() {
-  const client = await createAuth0Client(auth0Config); // ✅ idem ici
+  const client = await createAuth0Client(auth0Config);
 
   try {
     await client.handleRedirectCallback();
@@ -51,7 +51,7 @@ export async function handleRedirectCallback() {
 }
 
 export async function loginUserWithRedirect() {
-  const client = await window.createAuth0Client(auth0Config); // ✅ idem ici
+  const client = await createAuth0Client(auth0Config);
 
   sessionStorage.setItem("postLoginRedirect", window.location.pathname);
 
