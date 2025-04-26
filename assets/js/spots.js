b00451c1d08454a89c392cda57d91cc61dae8a02
Sam Toledano <samueltoledano94@gmail.com>
2025-04-26 10:34:48 +0200

    retour_hier

--- assets/js/spots.js
+++ assets/js/spots.js
@@ -963,8 +963,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
       // 🔥 Récupérer tous les lieux sélectionnés
       const selectedPlaces = markers
-        .filter(marker => marker.fullRecord)
-        .map(marker => marker.fullRecord.id); // ⚡ ici on utilise le Nom du lieu (peut-être ID si tu en as un)
+      .filter(marker => marker.fullRecord)
+      .map(marker => marker.fullRecord.name); // ⚡ ici on utilise le Nom du lieu (peut-être ID si tu en as un)
 
       if (selectedPlaces.length === 0) {
         alert("Select at least one spot to create a tour.");
